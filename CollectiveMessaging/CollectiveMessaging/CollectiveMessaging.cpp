2278804aed5a02df60c435ce91ee40a09147668f
Danil Lans <danimen0001@gmail.com>
2021-04-07 19:53:53 +0300

    lab 3 + start lab 3 dop matrix

--- CollectiveMessaging/CollectiveMessaging/CollectiveMessaging.cpp
+++ CollectiveMessaging/CollectiveMessaging/CollectiveMessaging.cpp
@@ -12,14 +12,17 @@ void Messaging(int M)
 	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
 	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
 
-	while (M > 0) {
-		if (ProcRank == 0) {
+	while (M > 0) 
+	{
+		if (ProcRank == 0)
+		{
 			cout << "Loop: " << currentLoop - M << endl;
 			MPI_Send(&ProcRank, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
 			MPI_Recv(&RecvRank, 1, MPI_INT, ProcNum - 1, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
 			cout << "#1 process: " << ProcRank << " from: " << RecvRank << endl;
 		}
-		else {
+		else 
+		{
 			MPI_Recv(&RecvRank, 1, MPI_INT, ProcRank - 1, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
 			cout << "Process: " << ProcRank << " from: " << RecvRank << endl;
 
@@ -40,24 +43,27 @@ void CollectiveMessaging(int M)
 	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
 	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
 
-
-	while (M != 0) {
-
-		if (ProcRank == 0) {
+	while (M != 0)
+	{
+		if (ProcRank == 0)
+		{
 			cout << "Loop: " << currentLoop - M << endl;
 			SendRank = ProcRank + 1;
 			RecvRank = ProcNum - 1;
 			MPI_Bcast(&SendRank, 1, MPI_INT, ProcRank, MPI_COMM_WORLD);
 			cout << "Senior process: " << ProcRank << " recieve message from: " << RecvRank << endl;
 		}
-		else {
-			if (ProcRank == ProcNum - 1) {
+		else
+		{
+			if (ProcRank == ProcNum - 1)
+			{
 				SendRank = 0;
 				RecvRank = ProcRank - 1;
 				MPI_Bcast(&SendRank, 1, MPI_INT, ProcRank, MPI_COMM_WORLD);
 				cout << "Junior process: " << ProcRank << " recieve message from: " << RecvRank << endl;
 			}
-			else {
+			else
+			{
 				SendRank = ProcRank - 1;
 				RecvRank = ProcRank + 1;
 				MPI_Bcast(&SendRank, 1, MPI_INT, ProcRank, MPI_COMM_WORLD);
@@ -66,12 +72,118 @@ void CollectiveMessaging(int M)
 		}
 		M--;
 		MPI_Barrier(MPI_COMM_WORLD);
+	}
+	if (ProcRank == 0)
+	{
+		cout << "End " << currentLoop << " loop of " << endl;
+	}
+}
 
+// lab 3
+void GeneralizedMessaging(int M)
+{
+	int ProcNum, ProcRank, RecvRank, SendRank, currentLoop = M + 1, message, totalSum = 0;
+	MPI_Status Status;
+
+	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+
+	int mas[4];
+	int mas2[4];
+
+	while (M != 0)
+	{
+		if (ProcRank == 0)
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				mas[i] = i;
+			}
+			cout << "Loop: " << currentLoop - M << endl;
+			SendRank = ProcRank + 1;
+			RecvRank = ProcNum - 1;
+			MPI_Scatter(&mas, 1, MPI_INT, &mas2, 1, MPI_INT, ProcRank, MPI_COMM_WORLD);
+			//cout << "Senior process: " << ProcRank << " recieve message from: " << RecvRank << endl;
+		}
+
+		else
+		{
+			if (ProcRank == ProcNum - 1)
+			{
+				SendRank = 0;
+				RecvRank = ProcRank - 1;
+				MPI_Scatter(&SendRank, 1, MPI_INT, &RecvRank, 1, MPI_INT, ProcRank, MPI_COMM_WORLD);
+				cout << "Junior process: " << ProcRank << " recieve message from: " << RecvRank << endl;
+			}
+			else
+			{
+				SendRank = ProcRank - 1;
+				RecvRank = ProcRank + 1;
+				MPI_Scatter(&SendRank, 1, MPI_INT, &RecvRank, 1, MPI_INT, ProcRank, MPI_COMM_WORLD);
+				cout << "Junior process: " << ProcRank << " recieve message from: " << SendRank << endl;
+			}
+			M--;
+			MPI_Barrier(MPI_COMM_WORLD);
+		}
 	}
 
+	if (ProcRank == 0) 
+	{
+		cout << "End " << currentLoop << " loop of " << endl;
+	}
+}
 
+// lab 3 dop matrix
+void GeneralizedMessagingMatrix(int M)
+{
+	int ProcNum, ProcRank, RecvRank, SendRank, currentLoop = M + 1, message, totalSum = 0;
+	MPI_Status Status;
 
-	if (ProcRank == 0) {
+	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+
+	int mas[4];
+	int mas2[4];
+
+	while (M != 0) 
+	{
+		if (ProcRank == 0) 
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				mas[i] = i;
+			}
+
+			cout << "Loop: " << currentLoop - M << endl;
+			SendRank = ProcRank + 1;
+			RecvRank = ProcNum - 1;
+			MPI_Scatter(&mas, 1, MPI_INT, &mas2, 1, MPI_INT, ProcRank, MPI_COMM_WORLD);
+			//cout << "Senior process: " << ProcRank << " recieve message from: " << RecvRank << endl;
+		}
+		else
+		{
+
+		}
+
+		if (ProcRank == 0)
+		{
+			cout << "Jun:" << "mas 1: ";
+			for (int i = 0; i < 4; i++)
+			{
+				cout << mas[i] << " ";
+			}
+			cout << " mas 2: ";
+			for (int i = 0; i < 4; i++)
+			{
+				cout << mas2[i] << " ";
+			}
+		}
+		M--;
+		MPI_Barrier(MPI_COMM_WORLD);
+	}
+
+	if (ProcRank == 0) 
+	{
 		cout << "End " << currentLoop << " loop of " << endl;
 	}
 }
@@ -82,7 +194,9 @@ int main(int* argc, char** argv) {
 	MPI_Init(argc, &argv);
 
 	//Messaging(M);
-	CollectiveMessaging(M);
+	//CollectiveMessaging(M);
+	//GeneralizedMessaging(M);
+	GeneralizedMessagingMatrix(M);
 
 	MPI_Finalize();
 
